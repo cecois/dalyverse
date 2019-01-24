155a74ad37e5ea8e3cbeb43af361df9b4d4c5d1b
C.C. Miller <christopher.miller@ll.mit.edu>
2019-01-24 16:16:43 -0500

    timeline comp data fixed

--- site/v1/src/components/timeline.vue
+++ site/v1/src/components/timeline.vue
@@ -1,10 +1,37 @@
 <template>
 <div>
-  timeline
+  <h5>timeline</h5>
+<p><code>{{console.msg}}</code></p>
+<p><code>{{windows.time.begin}}</code></p>
+<p><code>{{windows.time.end}}</code></p>
 </div>
 </template>
 
 <script>
+export default {
+name:'Timeline',
+data() {
+return {
+  windows:{
+    time:{begin:null,end:null}
+    ,space:{bbox:null}
+  },
+console:{msg:null,throb:false}
+}//return
+},//data
+computed:{},//computed
+watch:{},//watch
+methods:{},//methods
+created() {
+  console.log(this.$route.params.tstart);
+  return null;
+  },//created
+  mounted: function () {}//mounted
+
+
+
+
+}//export.timeline
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
