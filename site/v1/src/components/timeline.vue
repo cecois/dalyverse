fbfc76e1c4104f485d3a7f70dee697ad9d175716
cecois <cecmcgee@gmail.com>
2019-01-26 06:55:36 -0500

    cleanup

--- site/v1/src/components/timeline.vue
+++ site/v1/src/components/timeline.vue
@@ -3,6 +3,7 @@
   <h1 class="is-size-1">timeline</h1>
 <p>console.msg:<code>{{console.msg}}</code></p>
 <p>console.throb:<code>{{console.throb}}</code></p>
+<p>filter:<code>{{filter}}</code></p>
 <p>windows.time.begin:<code>{{windows.time.begin}}</code></p>
 <p>windows.time.end:<code>{{windows.time.end}}</code></p>
 <p>windows.space:<code>{{windows.space.bbox}}</code></p>
@@ -14,6 +15,7 @@ export default {
 name:'Timeline',
 data() {
 return {
+  filter:null,
   windows:{
     time:{begin:null,end:null}
     ,space:{bbox:null}
@@ -28,6 +30,7 @@ created() {
   
   this.windows.time.begin=(typeof this.$route.params.tstart !== 'undefined')?this.$route.params.tstart:null;
   this.windows.time.end=(typeof this.$route.params.tend !== 'undefined')?this.$route.params.tend:null;
+  this.filter=(typeof this.$route.params.filter !== 'undefined')?this.$route.params.filter:null;
 
   return null;
   },//created
