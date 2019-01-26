dbb7e5ce6f65c2ea25532e94a43875865a21e7f9
cecois <cecmcgee@gmail.com>
2019-01-26 16:09:42 -0500

    timeline frm vis.js added

--- site/v1/src/components/timeline.vue
+++ site/v1/src/components/timeline.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/vis/4.17.0/vis.min.css'><link rel='stylesheet' href="https://visjs.org/dist/vis-timeline-graph2d.min.css">
   <h1 class="is-size-1">timeline</h1>
 <p>console.msg:<code>{{console.msg}}</code></p>
 <p>console.throb:<code>{{console.throb}}</code></p>
@@ -7,14 +8,42 @@
 <p>windows.time.begin:<code>{{windows.time.begin}}</code></p>
 <p>windows.time.end:<code>{{windows.time.end}}</code></p>
 <p>windows.space:<code>{{windows.space.bbox}}</code></p>
+
+<div id="line"></div>
+
 </div>
 </template>
 
 <script>
+
+  // DOM element where the Timeline will be attached
+var container = document.getElementById("line");
+
+// Create a DataSet (allows two way data-binding)
+var items = new vis.DataSet([
+  { id: 1, content: "item 1", start: "2017-04-20" },
+  { id: 2, content: "item 2", start: "2017-04-14" },
+  { id: 3, content: "item 3", start: "2017-04-18" },
+  { id: 4, content: "item 4", start: "2017-04-16", end: "2017-04-19" },
+  { id: 5, content: "item 5", start: "2017-04-25" },
+  { id: 6, content: "item 6", start: "2017-04-27", type: "point" }
+]);
+
+// Configuration for the Timeline
+var options = {};
+
 export default {
 name:'Timeline',
 data() {
 return {
+  times:[
+  { id: 1, content: "time.1", start: "1912-04-20" },
+  { id: 2, content: "time.2", start: "2017-04-14" },
+  { id: 3, content: "time.3", start: "2017-04-18" },
+  { id: 4, content: "time.4", start: "2017-04-16", end: "2017-04-19" },
+  { id: 5, content: "time.5", start: "2017-04-25" },
+  { id: 6, content: "time.6", start: "2019-01-27", type: "point" }
+],
   filter:null,
   windows:{
     time:{begin:null,end:null}
@@ -34,7 +63,20 @@ created() {
 
   return null;
   },//created
-  mounted: function () {}//mounted
+  mounted: function () {
+
+/* ---------------------------------- TIMELINE -------------------- */
+
+this.$nextTick(() => {
+       // get the element
+       const el = this.$el.querySelector('#line')
+       // create the Timeline
+       this.timeline = new vis.Timeline(el, this.times, options);
+     })
+
+/* ---------------------------------- /TIMELINE */
+
+  }//mounted
 
 
 
@@ -44,5 +86,9 @@ created() {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
+#mynetwork {
+    width: 600px;
+    height: 400px;
+    border: 1px solid lightgray;
+}
 </style>
